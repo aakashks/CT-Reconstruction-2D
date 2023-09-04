47eb0405581c5d29fbf1d97f4f2f8ca70ef07332
aakashks <114357454+aakashks@users.noreply.github.com>
2023-09-04 16:51:25 +0530

    shifted plotting func

--- core.py
+++ core.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 
 
@@ -157,29 +155,3 @@ class SolveEquation:
             self.x = self.gauss_elimination(self.A, self.b)
 
         return self.x
-
-
-# Plotting Utility functions
-def plot_image(img):
-    sns.heatmap(img, cmap='viridis', xticklabels=False, yticklabels=False)
-    plt.tight_layout()
-    plt.show()
-
-
-def plot_images(img1, img2, show_rmse=True, figsize=(12, 5)):
-    fig, axes = plt.subplots(1, 2, figsize=figsize)
-
-    # Rescaling both images
-    [img1, img2] = [(x - x.min()) / (x.max() - x.min()) for x in [img1, img2]]
-
-    for ax, img in zip(axes, [img1, img2]):
-        sns.heatmap(img, cmap='viridis', ax=ax, xticklabels=False, yticklabels=False)
-
-    fig.tight_layout()
-
-    if show_rmse:
-        total_pixels = img1.size
-        rmse = np.sqrt(np.mean((img1 - img2) ** 2)) / total_pixels
-        plt.suptitle(f'RMSE: {rmse:.4f}', y=1.02)
-
-    plt.show()
